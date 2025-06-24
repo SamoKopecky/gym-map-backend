4c4ec512c82a68904c77f6252a297f6362d47e79
Samuel Kopecky <samuel.kopecky@heureka.group>
2025-06-24 21:13:17 +0200

    temp
    Signed-off-by: Samuel Kopecky <samuel.kopecky@heureka.group>

--- crud/exercise.go
+++ crud/exercise.go
@@ -15,6 +15,18 @@ func NewExercise(db bun.IDB) Exercise {
 	return Exercise{CRUDBase: CRUDBase[model.Exercise]{db: db}}
 }
 
+const getWithCountQuery = `
+WITH instruction_counts AS (
+	 select count(instruction.id) as c, instruction.exercise_id from exercise join instruction on instruction.exercise_id = exercise.id group by instruction.exercise_id
+) 
+SELECT exercise.*, instruction_counts.c FROM exercise LEFT JOIN instruction_counts on exercise.id = instruction_counts.exercise_id
+`
+
+func (e Exercise) GetWithCount() (exercises []model.Exercise, err error) {
+	err = e.db.NewRaw()
+
+}
+
 func (e Exercise) GetByMachineId(machineId int) (exercises []model.Exercise, err error) {
 	err = e.db.NewSelect().
 		Model(&exercises).
